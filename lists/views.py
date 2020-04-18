1ab0161dd7997a2ceb5e7624ebc8d8187038be12
SilenZen <243232325@qq.com>
2020-04-18 22:30:43 +0800

    Refactor home page view to use a tempalte

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home_page(request):
     # self.assertTrue(response.contentt.startswith(b'<html>'))
-    return HttpResponse(b'<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
